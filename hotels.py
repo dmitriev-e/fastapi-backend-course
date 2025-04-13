402b432aaf11ec14c53299e4b4d0edc72edf95c3
D-Ev_io <git@d-ev.io>
2025-04-13 19:13:51 +0300

    Part #5.1: changed test list of hotels to list of Pydantic objects

--- hotels.py
+++ hotels.py
@@ -7,23 +7,22 @@ logger = logging.getLogger("uvicorn")
 
 router = APIRouter(prefix="/hotels", tags=["Hotels"])
 
-# Create dict hotels [{id: star, name: str, city: str, stars: int}]
 hotels = [
-    {"id": 1, "name": "The Ritz-Carlton", "city": "New York", "stars": 5},
-    {"id": 2, "name": "The Westin", "city": "San Francisco", "stars": 4},
-    {"id": 3, "name": "The Sheraton", "city": "Los Angeles", "stars": 3},
-    {"id": 4, "name": "The Hilton", "city": "Chicago", "stars": 5},
-    {"id": 5, "name": "The Holiday Inn", "city": "Miami", "stars": 3},
-    {"id": 6, "name": "The Hyatt Regency", "city": "Las Vegas", "stars": 4},
-    {"id": 7, "name": "The Park Hyatt", "city": "Tokyo", "stars": 5},
-    {"id": 8, "name": "The Grand Hyatt", "city": "Seoul", "stars": 4},
-    {"id": 9, "name": "The InterContinental", "city": "London", "stars": 5},
-    {"id": 10, "name": "The Sofitel", "city": "Paris", "stars": 4},
-    {"id": 11, "name": "The Marriott", "city": "Berlin", "stars": 4},
-    {"id": 12, "name": "The Renaissance", "city": "Rome", "stars": 4},
-    {"id": 13, "name": "The Courtyard by Marriott", "city": "Amsterdam", "stars": 3},
-    {"id": 14, "name": "The W Hotel", "city": "Barcelona", "stars": 5},
-    {"id": 15, "name": "The Sheraton", "city": "Stockholm", "stars": 4},
+    Hotel(id=1, name="The Ritz-Carlton", city="New York", stars=5),
+    Hotel(id=2, name="The Westin", city="San Francisco", stars=4),
+    Hotel(id=3, name="The Sheraton", city="Los Angeles", stars=3),
+    Hotel(id=4, name="The Hilton", city="Chicago", stars=5),
+    Hotel(id=5, name="The Holiday Inn", city="Miami", stars=3),
+    Hotel(id=6, name="The Hyatt Regency", city="Las Vegas", stars=4),
+    Hotel(id=7, name="The Park Hyatt", city="Tokyo", stars=5),
+    Hotel(id=8, name="The Grand Hyatt", city="Seoul", stars=4),
+    Hotel(id=9, name="The InterContinental", city="London", stars=5),
+    Hotel(id=10, name="The Sofitel", city="Paris", stars=4),
+    Hotel(id=11, name="The Marriott", city="Berlin", stars=4),
+    Hotel(id=12, name="The Renaissance", city="Rome", stars=4),
+    Hotel(id=13, name="The Courtyard by Marriott", city="Amsterdam", stars=3),
+    Hotel(id=14, name="The W Hotel", city="Barcelona", stars=5),
+    Hotel(id=15, name="The Sheraton", city="Stockholm", stars=4),
 ]
 
 @router.get("/")
@@ -36,11 +35,11 @@ async def get_hotels(
 
     return_data = []
     for hotel in hotels:
-        if name and name.lower() not in hotel["name"].lower():
+        if name and name.lower() not in hotel.name.lower():
             continue
-        if hotel_id and hotel_id != hotel["id"]:
+        if hotel_id and hotel_id != hotel.id:
             continue
-        if city and city.lower() not in hotel["city"].lower():
+        if city and city.lower() not in hotel.city.lower():
             continue
         return_data.append(hotel)
     return return_data
@@ -51,7 +50,7 @@ async def create_hotel(hotel_data: HotelCreateData) -> dict:
     """ Create new hotel """
 
     new_hotel = Hotel(
-        id = max([hotel["id"] for hotel in hotels]) + 1,
+        id = max([hotel.id for hotel in hotels]) + 1,
         name = hotel_data.name,
         stars = hotel_data.stars,
         city = hotel_data.city,
@@ -68,10 +67,10 @@ async def edit_hotel(
     """ Update hotel with full parameters list """
 
     for hotel in hotels:
-        if hotel["id"] == hotel_id:
-            hotel["name"] = hotel_data.name
-            hotel["stars"] = hotel_data.stars
-            hotel["city"] = hotel_data.city
+        if hotel.id == hotel_id:
+            hotel.name = hotel_data.name
+            hotel.stars = hotel_data.stars
+            hotel.city = hotel_data.city
             return {"status": 200, "message": f"Hotel {hotel_id=} updated", "data": hotel}
         else:
             continue
@@ -86,13 +85,13 @@ async def update_hotel(
     """ Partial Update hotel by ID and partial parameters list """
 
     for hotel in hotels:
-        if hotel["id"] == hotel_id:
+        if hotel.id == hotel_id:
             if hotel_data.name:
-                hotel["name"] = hotel_data.name
+                hotel.name = hotel_data.name
             if hotel_data.stars:
-                hotel["stars"] = hotel_data.stars
+                hotel.stars = hotel_data.stars
             if hotel_data.city:
-                hotel["city"] = hotel_data.city
+                hotel.city = hotel_data.city
             return {"status": 200, "message": f"Hotel {hotel_id=} updated", "data": hotel}
         else:
             continue
@@ -105,7 +104,7 @@ async def delete_hotel(hotel_id: int) -> dict:
 
     for hotel in hotels:
         logger.info(hotel)
-        if hotel["id"] == hotel_id:
+        if hotel.id == hotel_id:
             hotels.remove(hotel)
             return {"status": 200, "message": f"Hotel {hotel_id=} deleted"}
         else:
